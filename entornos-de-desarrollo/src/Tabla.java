26f45617fed708c38e8a3c3f82f078e55ef3983e
mateorussi <73946312+mateorussi@users.noreply.github.com>
2021-03-11 12:25:49 +0100

    11-03-2021 (2)

---
+++ entornos-de-desarrollo/src/Tabla.java
@@ -0,0 +1,28 @@
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+/**
+ * 
+ */
+
+/**
+ * @author mateorussi
+ *
+ */
+public class Tabla {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		JTable tabla=new JTable();
+		
+		JScrollPane tablaScroll=new JScrollPane();
+		
+		
+		System.out.println(tabla);
+	}
+
+}
