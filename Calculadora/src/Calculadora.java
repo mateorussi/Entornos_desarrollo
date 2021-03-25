147a9561ed1a5e4173516f4d5fae3ae561f433dd
mateorussi <73946312+mateorussi@users.noreply.github.com>
2021-03-25 14:00:42 +0100

    25-03-21

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author mateorussi
+ *
+ */
+public class Calculadora {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Interfaz-Grafico/src/Grafico1.java
+++ Interfaz-Grafico/src/Grafico1.java
@@ -1,10 +1,20 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class Grafico1 {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -36,6 +46,52 @@ public class Grafico1 {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("DEPARTAMENTOS");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(0, 25, 450, 16);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(39, 66, 355, 128);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nombre Departamento:");
+		lblNewLabel_1.setBounds(23, 24, 147, 16);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Código Departamento:");
+		lblNewLabel_1_1.setBounds(23, 59, 147, 16);
+		panel.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Localidad Departamento:");
+		lblNewLabel_1_2.setBounds(23, 95, 161, 16);
+		panel.add(lblNewLabel_1_2);
+		
+		textField = new JTextField();
+		textField.setBounds(196, 19, 130, 26);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(196, 54, 130, 26);
+		panel.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(196, 90, 130, 26);
+		panel.add(textField_2);
+		
+		JButton btnNewButton = new JButton("INSERTAR DATOS");
+		btnNewButton.setBounds(49, 216, 153, 29);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnBorrarDatos = new JButton("BORRAR DATOS");
+		btnBorrarDatos.setBounds(228, 216, 153, 29);
+		frame.getContentPane().add(btnBorrarDatos);
 	}
-
 }
